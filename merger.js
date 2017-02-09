da1b845a393db1ddb411c6b046fdd743bbd90559
Nick Lee <nicklee808@gmail.com>
2017-02-08 16:02:07 -1000

    merge sort finished

--- merger.js
+++ merger.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-module.exports = function(inputArray){
+module.exports = function merger(inputArray){
   
   if(inputArray instanceof Array === false){
     return `Received ${typeof inputArray} while expecting array`;
@@ -16,6 +16,39 @@ module.exports = function(inputArray){
     return inputArray;
   }
   
-  let mid = Math.floor(inputArray.length / 2);
+  let leftArray = merger(inputArray.slice(0, Math.floor(inputArray.length / 2))),
+    rightArray = merger(inputArray.slice(Math.floor(inputArray.length / 2)));
+
+  leftArray = merger(leftArray);
+  rightArray = merger(rightArray);
+
+  return merge(leftArray, rightArray);
 
 };
+
+function merge(leftArray, rightArray){
+  let returnArray = [];
+
+  while(leftArray.length > 0 && rightArray.length > 0 ){
+    if (leftArray[0] > rightArray[0]){
+      returnArray.push(rightArray[0]);
+      rightArray.splice(0, 1);
+    }else{
+      returnArray.push(leftArray[0]);
+      leftArray.splice(0, 1);
+    }
+  }
+
+  while (leftArray.length > 0){
+    returnArray.push(leftArray[0]);
+    leftArray.splice(0, 1);
+  }
+
+  while (rightArray.length > 0){
+    returnArray.push(rightArray[0]);
+    rightArray.splice(0, 1);
+  }
+
+  return returnArray;
+
+}
